da0d9bd300057752794d64865dae1b4ce30b2de5
MysteryLEE <457157005@qq.com>
2023-10-14 15:29:17 -0700

    temp1-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,5 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    int gcdByEuclidsAlgorithm(int n1, int n2) {
-        if (n2 == 0) {
-            return n1;
-        }
-        return gcdByEuclidsAlgorithm(n2, n1 % n2);
-    }
 
 }
