8d96cec8dd79c83c3c1d252c67ed4285fcd24e64
MysteryLEE <457157005@qq.com>
2023-10-15 14:38:13 -0700

    Program-temp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
-    public static float Sum(float[] data) {
-
-        float sum = 0;
-
-        for (int i = 0; i < data.length; i++) {
-
-            sum += data[i];
-
-        }
-
-        return sum;
-
-    }
-
 }
