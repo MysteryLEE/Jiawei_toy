c6f2db1862f03dac4ca01e61a345dbe28bb5ff74
MysteryLEE <457157005@qq.com>
2023-10-15 14:16:14 -0700

    Program-hm-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    protected void modify(Transaction t) {
+        try {
+            this.lock.writeLock().lock();
+            t.perform();
+        } finally {
+            this.lock.writeLock().unlock();
+        }
+
+    }
 }
