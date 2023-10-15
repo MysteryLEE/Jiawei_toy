4643ee9dd6839696c479cc9e008467b404f34635
MysteryLEE <457157005@qq.com>
2023-10-15 14:24:33 -0700

    Program-temp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
-    public void executeWithWriteLock(Runnable t) {
-        writeLock.lock();
-        try {
-            t.run();
-        } finally {
-            writeLock.unlock();
-        }
-    }
 }
