12b7a44934f3cb3aa4d046f7d69e10d9c3530b8c
MysteryLEE <457157005@qq.com>
2023-10-15 14:40:10 -0700

    Program-temp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
-    public static double entropy(double[] values) {
-        double entropy = 0;
-        int total = 0;
-        for (double value : values) {
-            if (value > 0) {
-                entropy += value * Math.log(value);
-                total++;
-            }
-        }
-        if (total > 0) {
-            entropy = -entropy / Math.log(total);
-        }
-        return entropy;
-    }
-
 }
