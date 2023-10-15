21540204b6a341d180120ba71d10b904fb10c1d3
MysteryLEE <457157005@qq.com>
2023-10-15 14:40:48 -0700

    Program-hm-39

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,32 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static double Entropy( int[] values ){
+        int     n = values.length;
+        int     total = 0;
+        double  entropy = 0;
+        double  p;
+
+        // calculate total amount of hits
+        for ( int i = 0; i < n; i++ )
+        {
+            total += values[i];
+        }
+
+        if ( total != 0 )
+        {
+            // for all values
+            for ( int i = 0; i < n; i++ )
+            {
+                // get item's probability
+                p = (double) values[i] / total;
+                // calculate entropy
+                if ( p != 0 )
+                    entropy += ( -p * (Math.log10(p)/Math.log10(2)) );
+            }
+        }
+        return entropy;
+    }
+
 }
