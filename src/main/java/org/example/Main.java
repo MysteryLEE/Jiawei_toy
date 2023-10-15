146fc2156c4c1f2aff3f345cceab79ff81978b15
MysteryLEE <457157005@qq.com>
2023-10-15 14:26:01 -0700

    Program-ai-17

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,4 +6,12 @@ public class Main {
     }
 
 
+    public static int sum(int[] data) {
+        int result = 0;
+        for (int value : data) {
+            result += value;
+        }
+        return result;
+    }
+
 }
